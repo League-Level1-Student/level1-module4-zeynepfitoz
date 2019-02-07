e160378a5081ead81fab791ae38bd7e8e9b76b0e
league <league@wts9.attlocal.net>
2019-02-06 18:25:54 -0800

    illusions

--- src/BookOfIllusions.java
+++ src/BookOfIllusions.java
@@ -21,7 +21,9 @@ public class BookOfIllusions extends MouseAdapter {
 	 * Here we are instantiating our BookOfIllusions class and calling it’s createBook() method. This is because we want to get out of the
 	 * static main method, so that we can add a click listener to each illusion.
 	 */
-
+	JLabel label;
+	JLabel label2;
+	JLabel label3;
 	public static void main(String[] args) throws Exception {
 		BookOfIllusions illusions = new BookOfIllusions();
 		illusions.createBook();
@@ -37,28 +39,35 @@ JFrame one = new JFrame();
 		// 4. find 2 images and save them to your project’s default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
 		String image = "dolantwins.jpg";
-		String image2 = "dolantwins2.jpg";
+		String image2 = "dolantwins2.jpeg";
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
-		JLabel label;
-		JLabel label2;
+
 		// 7. use the "loadImage..." methods below to initialize your JLabel
 		label = loadImageFromComputer(image);
 		label2 = loadImageFromComputer(image2);
 		// 8. add your JLabel to the frame
 		one.add(label);
-		one.add(label2);
+		
 		// 9. call the pack() method on the frame
 		one.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
-		System.out.println(one.addMouseListener(this));
+		one.addMouseListener(this);
+
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
-		
+		System.out.println("clicked");
 		// 12. remove everything from the frame that was added earlier
+
+	one.remove(label);
+	one.add(label2);
 		// 13. load a new image like before (this is more than one line of code)
+	String image3 = "ducklings.jpg";
+	label3 = loadImageFromComputer(image3);
+	
 		// 14. pack the frame
+	one.pack();
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
