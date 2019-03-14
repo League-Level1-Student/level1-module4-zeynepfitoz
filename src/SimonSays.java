321800dbcb6cab0821e2b4424e08e2b7f52c943b
league <league@wts9.attlocal.net>
2019-03-13 18:25:18 -0700

    simonsays in progress

--- src/SimonSays.java
+++ src/SimonSays.java
@@ -22,7 +22,7 @@ public class SimonSays extends KeyAdapter {
 
 	// Complete steps 1 - 7 before you test
 	// 1. Declare a JFrame variable
-	
+	JFrame frame = new JFrame();
 	HashMap<Integer, String> images = new HashMap<Integer, String>();
 	private int imageIndex;
 	private int tries = 0;
@@ -32,11 +32,15 @@ public class SimonSays extends KeyAdapter {
 	private void makeAlbum() {
 		// 2. Add the four images that match keyboard keys like this: 
 		//images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
-
+		images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
+		images.put(new Integer(KeyEvent.VK_UP), "right.jpg");
+		images.put(new Integer(KeyEvent.VK_UP), "down.jpg");
+		images.put(new Integer(KeyEvent.VK_UP), "right.jpg");
 		// 3. Use a JOptionPane to tell the user the rules: "Press the matching key when
 		// 'Simon says' otherwise press a different key"
-		
+		JOptionPane.showMessageDialog(null, "Press the matching key when 'Simon says' otherwise press a different key");
 		// 4. Call the showImage method to show an image
+		showImage();
 	}
 
 	public void keyPressed(KeyEvent e) {
@@ -69,22 +73,22 @@ public class SimonSays extends KeyAdapter {
 
 	private void showImage() {
 		// 5. Initialize your frame to a new JFrame()
-		
+		frame = new JFrame();
 		// 6. Set the frame to visible
-
+frame.setVisible(true);
 		// 7. Uncomment the following line to add a random image to your frame
-		//frame.add(getNextRandomImage());
+		frame.add(getNextRandomImage());
 
 		// 8. Set the name of your frame
-
+frame.setName("dolan");
 		// 9. Pack the frame
-		
-		// 10. Set the defaultCloseOperation of your from to JFrame.EXIT_ON_CLOSE
-		
+		frame.pack();
+		// 10. Set the defaultCloseOperation of your frame to JFrame.EXIT_ON_CLOSE
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 11. Add a key listener to the frame
-
+frame.addKeyListener(this);
 		// 12. Create a new instance of Random
-
+Random randy = new Random();
 		// 13. Use the Random and the speak method to either say 
 		// "Simon says press this key" or "Press this key"
 
